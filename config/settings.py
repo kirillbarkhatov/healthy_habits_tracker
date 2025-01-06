eac9b2f7edc8de5fd209cdb5087b87edabb7103d
kirill_barkhatov <k.s.barkhatov@gmail.com>
2025-01-07 00:15:40 +0300

    базовые настройки и модель привычки

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i8ot%+0y30k%naefojqldr9r(uxu2y+nm_7azud0f^%2wfw0!g"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "habits",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +83,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER_NAME"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
 
@@ -103,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
@@ -121,3 +133,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
---
+++ habits/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from datetime import datetime
+
+from config.settings import AUTH_USER_MODEL
+
+
+class Habit(models.Model):
+    """Модель привычки"""
+
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Пользователь")
+    location = models.CharField(max_length=50, default="Где угодно", verbose_name="Место")
+    time = models.TimeField(verbose_name="Время")
+    action = models.CharField(max_length=50, verbose_name="Действие")
+    is_pleasant_habit = models.BooleanField(default=False, verbose_name="Признак приятной привычки")
+    related_habit = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Связанная привычка")
+    periodicity = models.IntegerField(default=1, verbose_name="Периодичность в днях")
+    award = models.CharField(max_length=50, null=True, blank=True, verbose_name="Вознаграждение")
+    execution_time = models.IntegerField(default=0, verbose_name="Время выполнения (в секундах)")
+    is_public = models.BooleanField(default=False, verbose_name="Признак публичности")
+
+    def __str__(self):
+        return f"{self.user} - {self.action}"
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
+        ordering = [
+            "pk",
+        ]
