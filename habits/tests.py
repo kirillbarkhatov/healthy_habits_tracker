6dd5068ddea45d58f020bca92ec5e93a6caafd57
kirill_barkhatov <k.s.barkhatov@gmail.com>
2025-01-13 16:45:01 +0300

    Первый тест

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from datetime import time
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from habits.models import Habit
+from users.models import User
+
+class HabitTestCase(APITestCase):
+    def setUp(self):
+        # Подготовка данных перед каждым тестом
+
+        self.user = User.objects.create(
+            email="test1@test1.ru",
+        )
+
+        self.habit = Habit.objects.create(
+            time=time(12, 0),
+            action="Полезная привычка",
+            user=self.user
+        )
+
+        self.client.force_authenticate(user=self.user)
+
+
+    def test_habit_list(self):
+        url = reverse("habits:habit_list")
+        response = self.client.get(url)
+        data = response.json()
+        result = {
+            "count": 1,
+            "next": None,
+            "previous": None,
+            "results": [
+                {
+                    "id": self.habit.pk,
+                    "execution_time": 0,
+                    "periodicity": 1,
+                    "location": "Где угодно",
+                    "time": "12:00:00",
+                    "action": "Полезная привычка",
+                    "is_pleasant_habit": False,
+                    "award": None,
+                    "is_public": False,
+                    "user": self.user.pk,
+                    "related_habit": None
+                }
+            ]
+        }
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, result)
